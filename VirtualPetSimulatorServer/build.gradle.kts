b0484bcd5678676aa4df5644fd3271b819ec2e30
Casiana <88460309+casiana2075@users.noreply.github.com>
2024-06-01 15:55:19 +0300

    Upload resources

--- VirtualPetSimulatorServer/build.gradle.kts
+++ VirtualPetSimulatorServer/build.gradle.kts
@@ -1,19 +1,38 @@
 plugins {
-    id("java")
+    java
+    id("org.springframework.boot") version "3.2.5"
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.postgresql:postgresql:42.7.3")
+    implementation("javax.persistence:javax.persistence-api:2.2")
+    implementation("org.hibernate:hibernate-core:6.5.1.Final")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
-tasks.test {
+tasks.withType<Test> {
     useJUnitPlatform()
 }
---
+++ VirtualPetSimulatorServer/src/main/java/org/example/services/UsersService.java
@@ -0,0 +1,53 @@
+package org.example.services;
+
+import org.example.Result;
+import org.example.entities.User;
+import org.example.repositories.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsersService {
+    @Autowired
+    private UsersRepository usersRepository;
+
+    public Result<User> create(String username, String email, String password) {
+        User user = new User(username, email, password);
+        try {
+            User newUser = usersRepository.save(user);
+            return Result.success(newUser);
+        } catch (Exception e) {
+            return Result.failure("DuplicateUsernameOrEmail");
+        }
+    }
+
+    public Result<User> findByIdentifier(String identifier) {
+        User user = usersRepository.findByUsername(identifier);
+        if (user != null) {
+            return Result.success(user);
+        }
+        user = usersRepository.findByEmail(identifier);
+        if (user != null) {
+            return Result.success(user);
+        }
+        return Result.failure("UserNotFound");
+    }
+
+    public User updateScore(int userId, int score) {
+        User user = usersRepository.findById(userId).orElse(null);
+        if (user == null) {
+            return null;
+        }
+        user.setScore(user.getScore() + score);
+        return usersRepository.save(user);
+    }
+
+    public User updateCurrency(int userId, int currency) {
+        User user = usersRepository.findById(userId).orElse(null);
+        if (user == null) {
+            return null;
+        }
+        user.setCurrency(user.getCurrency() + currency);
+        return usersRepository.save(user);
+    }
+}
